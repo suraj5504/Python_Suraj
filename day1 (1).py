3fb93be6ad5d7244914b1a1d6a9db8ccdc381b6e
Suraj5504 <maitysuraj022@gmail.com>
2021-10-22 09:58:35 +0530

    Create day1 (1).py

---
+++ day1 (1).py
@@ -0,0 +1,86 @@
+#Rules to declare the variable
+'''
+1) Must start with the letter
+2) Cannot start with a numb
+3) A variable can only alpha-numeric char
+4) variable are case sensitive( name, Name)
+5) variable name cannot same as your reserve - keyword
+'''
+#Keywords in python
+'''
+False	await	else	import	pass
+None	break	except	in	raise
+True	class	finally	is	return
+and 	continue for	lambda	try
+as	    def	    from	nonlocal	while
+assert	del	    global	not	with
+async	elif	if	or	yield
+'''
+name = "Harry"
+name_1 = "Sam"
+# False = "Sam"
+# True = "value"
+
+true = "hello world"
+false = "hello world"
+
+# num1 = 1
+# num2 = 2
+# num3 = 3
+num1,num2,num3 = 1 , 2 , 3
+
+# print(num1,num2,num3)
+
+name,age,city = "Sam",22,"mumbai"
+# print(name, age, city)
+Fname,Lname,college,branch="Wajid","Shaikh","Terna engineering college","Mechanical"
+
+
+# print("Hello world")
+
+# #the value for num1 is 1. value for num2 is 2. value for num3 is 3.
+
+# print("The value for num1 is ",num1,"value for num2 is ",num2, "value of num3 is ",num3)
+
+
+# #My name is xyz i am studying in clg xyz i am Xyrs old i live in xyz
+# print("my name is ",Fname,"I am studying in ", college,"I am ",age,"year old","l live in ",city)
+
+# print(f"My name is {Fname}. I am studying in {college}. I am {age}yrs old. I live in {city}")
+
+
+#Input
+
+# name = input("enter your name")
+# num = int(input("enter your name :-"))
+# print(num)
+# #num1num2
+
+# num1 = int(input("Enter the num1 :- "))
+# num2 = int(input("Enter the num2 :- "))
+
+# print("Addition of two num is", num1+num2)
+
+# number1 = 10
+# number1 = 45
+# print(number1)
+
+# Name=input("enter Name")
+# Collage=input("enter College Name")
+# Age=int(input("enter Age"))
+# print(Name,Collage,Age)
+
+
+str1 = "Good "
+str2 =  25
+
+print(str1 * str2)
+
+
+'''
+python data types:-
+
+@linkdin
+Github
+
+'''
